93c033fdfefd273d8e25ffca5b8d4fc3f7c46c1c
Paulo Cesar <p.cesarofc@gmail.com>
2022-01-18 17:03:07 -0300

    Adição de opção via EAN

--- src/components/InserirProduto.jsx
+++ src/components/InserirProduto.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Button from "./Button";
-import './InserirProduto.css'
-import axios from 'axios'
-
+import './InserirProduto.css';
+import axios from 'axios';
 
 export default function ({AdicionarProduto}, {produtos}) {
-    const url = 'https://xpenses-121a5-default-rtdb.firebaseio.com/consultas/consultancm/produto.json?orderBy="ncm"&equalTo='
-    
+    const urlncm = 'https://xpenses-121a5-default-rtdb.firebaseio.com/consultas/consultancm/produto.json?orderBy="ncm"&equalTo='
+    const urlean = 'https://xpenses-121a5-default-rtdb.firebaseio.com/consultas/consultaean/produto.json?orderBy="ean"&equalTo='
     const [inputProduto, setInputProduto] = React.useState('')
 
     function handleChangeInput (e) {
@@ -14,29 +13,40 @@ export default function ({AdicionarProduto}, {produtos}) {
     }
 
     function handleClickButton () {
-        const url1 = url + '"' + inputProduto + '"'
-        
-        axios.get(url1).then(response => {
-            var i
-            let novosProdutos = []
-            console.log(response.data)
-            
-            for(i in response.data){
-                novosProdutos = [...novosProdutos, 
-                    {   descricao: response.data[i].descricao,
-                        cest: response.data[i].cest,
-                        ncm: response.data[i].ncm,
-                        cst_icms: response.data[i].cst_icms
-                    }
+
+        if(inputProduto.length == 8){
+            const url1 = urlncm + '"' + inputProduto + '"'
+            axios.get(url1).then(response => {
+                var i
+                let novosProdutos = []
+                for(i in response.data){
+                    novosProdutos = [...novosProdutos, 
+                        {   descricao: response.data[i].descricao,
+                            cest: response.data[i].cest,
+                            ncm: response.data[i].ncm,
+                            cst_icms: response.data[i].cst_icms
+                        }
                     ]
-            }
-
-            AdicionarProduto(novosProdutos)
-        })
-        
+                }
+                AdicionarProduto(novosProdutos)
+            })}
+            else{
+                const url1 = urlean + '"' + inputProduto + '"'
+                axios.get(url1).then(response => {
+                    var i
+                    let novosProdutos = []
+                    for(i in response.data){
+                        novosProdutos = [...novosProdutos, 
+                            {   descricao: response.data[i].descricao,
+                                cest: response.data[i].cest,
+                                ncm: response.data[i].ncm,
+                                cst_icms: response.data[i].cst_icms
+                            }
+                        ]
+                }
+                AdicionarProduto(novosProdutos)
+            })}   
     }
-
-
     return <>
         <input type="text" className="Input" onChange={handleChangeInput} value={inputProduto}/>
         <Button onClick={handleClickButton}/>
--- src/components/Produto.jsx
+++ src/components/Produto.jsx
@@ -3,18 +3,16 @@ import './Produto.css'
 import './Tributos.css'
 
 
+
 export default function ({produtos}) {
 
-
     return <div className="Produto">
-        
-        
-        <div className="Card">
-            
-            <div className="description">{produtos.descricao}</div>
-            <div className="cest">Cest : {produtos.cest}</div>
-            <div className="cest">NCM: {produtos.ncm}</div>
-            <div className="cest">Icms: {produtos.cst_icms}</div>
-        </div>
+            <div className="Card">
+                <div className="description">Produto : {produtos.descricao}</div>
+                <div className="cest">Cest : {produtos.cest}</div>
+                <div className="cest">NCM : {produtos.ncm}</div>
+                <div className="cest">ICMS : {produtos.cst_icms}</div>
+            </div>  
     </div>
 }
+
